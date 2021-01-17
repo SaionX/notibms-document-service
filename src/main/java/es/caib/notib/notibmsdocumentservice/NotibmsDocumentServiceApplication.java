9ed4af2d0ecdd62eb37864a518267c3324e57d52
sion <sion.andreu@gmail.com>
2021-01-17 19:07:52 +0100

    Pujada inicial

---
+++ src/main/java/es/caib/notib/notibmsdocumentservice/NotibmsDocumentServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.notib.notibmsdocumentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibmsDocumentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotibmsDocumentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/caib/notib/notibmsdocumentservice/NotibmsDocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.notib.notibmsdocumentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotibmsDocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
